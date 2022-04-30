280d4f759c5b8396a561fe6cf4f67259f0c6e873
wasfyelbaz <wasfylbaz@gmail.com>
2022-04-30 06:37:43 +0200

    Added db initialization sql

--- StudentSubjectsManagementSystem/Database/init.sql
+++ StudentSubjectsManagementSystem/Database/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS admin(id integer PRIMARY KEY AUTOINCREMENT, username 
 
 CREATE TABLE IF NOT EXISTS student(id integer PRIMARY KEY, name text NOT NULL, password text NOT NULL, academic_year text NOT NULL, admin_id integer, foreign key(admin_id) references admin(id) ON delete cascade ON UPDATE no action);
 
-CREATE TABLE IF NOT EXISTS course(code integer PRIMARY KEY, hours integer NOT NULL, name text NOT NULL, max_no_of_stud integer NOT NULL, admin_id integer, foreign key(admin_id) references admin(id)ON delete cascade ON UPDATE no action);
+CREATE TABLE IF NOT EXISTS course(code integer PRIMARY KEY, hours integer NOT NULL, name text NOT NULL, max_num_of_students integer NOT NULL, admin_id integer, foreign key(admin_id) references admin(id)ON delete cascade ON UPDATE no action);
 
 CREATE TABLE IF NOT EXISTS prereq(course integer, req_course integer, PRIMARY KEY(course, req_course), FOREIGN KEY(course) REFERENCES course(code) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(req_course) REFERENCES course(code) ON DELETE CASCADE ON UPDATE NO ACTION);
 
