aa3c1f774ea6edef69bb9cddba66a64d849db6f1
wasfyelbaz <wasfylbaz@gmail.com>
2022-04-30 06:38:21 +0200

    Added the following functionalities: - check_if_student_exist() - insert_student()

--- StudentSubjectsManagementSystem/Database/DatabaseHandler.cpp
+++ StudentSubjectsManagementSystem/Database/DatabaseHandler.cpp
@@ -3,18 +3,24 @@
 DatabaseHandler::DatabaseHandler() {
     // set db location
     db_location = "StudentsSubjectsManagementSystem.db";
-    // create db
-    create_db();
-    // create tables
-    create_table();
 }
-
+DatabaseHandler::DatabaseHandler(sqlite3* DB) {
+    // set db location
+    db_location = "StudentsSubjectsManagementSystem.db";
+    this->DB = DB;
+}
+// returns connection object
+sqlite3* DatabaseHandler::get_db_connection()
+{
+    return this->DB;
+}
+// create database file
 void DatabaseHandler::create_db()
 {
     sqlite3_open(this->db_location, &this->DB);
     return;
 }
-
+// create tables
 void DatabaseHandler::create_table()
 {
     // init sql
@@ -44,4 +50,71 @@ void DatabaseHandler::create_table()
         std::cerr << e.what();
     }
     return;
+}
+// check if student exist in the database
+bool DatabaseHandler::check_if_student_exist(Student student)
+{
+    // get student id
+    std::string id = student.get_id();
+    // construct query
+    std::string sql("SELECT * FROM student WHERE id=" + id + ";");
+
+    std::cout << "Query: " << sql << std::endl;
+    // execute prepared query
+    int exec = sqlite3_prepare_v2(DB, sql.c_str(), -1, &result, NULL);
+    // check query status
+    if (exec != SQLITE_OK) {
+        std::cerr << "Error: Couldn't query the database" << std::endl;
+        //sqlite3_free(message_error);
+        return false;
+    }
+    // get results
+    exec = sqlite3_step(result);
+    // check if there is at least 1 row
+    if (exec == SQLITE_ROW) {
+        // found the student
+        printf("%s\n", sqlite3_column_text(result, 0));
+        return true;
+    }
+    else
+    {
+        // didn't find the student
+        std::cerr << "Info: Couldn't find a student with id=" << id << std::endl;
+        return false;
+    }
+
+    //while ((exec = sqlite3_step(result)) == SQLITE_ROW) {
+        // process row here
+    //    printf("%s\n", sqlite3_column_text(result, 1));
+    //}
+
+}
+// insert student in the database
+int DatabaseHandler::insert_student(Student student, int admin_id)
+{
+    char* message_error;
+
+    if (check_if_student_exist(student) == 1)
+    {
+        std::cerr << "Error: Student already exists" << std::endl;
+        return -1;
+    }
+    std::string id = student.get_id();
+    std::string student_name = student.get_name();
+    std::string student_password = student.get_password();
+    std::string academic_year = student.get_academic_year();
+    std::string str_admin_id = std::to_string(admin_id);
+
+    std::string sql("INSERT INTO student (id, name, password, academic_year, admin_id) VALUES(" + id + ", '" + student_name + "', '" + student_password + "', '" + academic_year + "', " + str_admin_id + ");");
+    std::cout << "Query: " << sql << std::endl;
+    int exec = sqlite3_exec(DB, sql.c_str(), NULL, 0, &message_error);
+    if (exec != SQLITE_OK) {
+        std::cerr << "Error: Couldn't insert student" << std::endl;
+        sqlite3_free(message_error);
+        return false;
+    }
+    else {
+        std::cout << "Info: Student inserted successfully" << std::endl;
+        return true;
+    }
 }
--- StudentSubjectsManagementSystem/Database/DatabaseHandler.h
+++ StudentSubjectsManagementSystem/Database/DatabaseHandler.h
@@ -2,17 +2,26 @@
 #include "sqlite3.h"
 #include <iostream>
 #include <string>
+#include "../Student/Student.h"
+#include "../Course/Course.h"
+
+//char* sql_escape(const char* str);
 
 class DatabaseHandler
 {
 private:
 	sqlite3* DB;
+	sqlite3_stmt* result;
 	const char* db_location;
 
 public:
 	DatabaseHandler();
+	DatabaseHandler(sqlite3*);
+	sqlite3* get_db_connection();
 	void create_db();
 	void create_table();
-	bool insert_student();
+	
+	bool check_if_student_exist(Student);
+	int insert_student(Student, int);
+	
 };
-
