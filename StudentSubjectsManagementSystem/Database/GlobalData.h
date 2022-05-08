4799c8c5c8c27696ad5aca4bf9f940e3678b8868
Mariam Osama <mariam.osama.1308.2002@outlook.com>
2022-05-08 22:42:26 +0200

    Added courses in gd

--- StudentSubjectsManagementSystem/Database/GlobalData.h
+++ StudentSubjectsManagementSystem/Database/GlobalData.h
@@ -5,9 +5,11 @@
 
 class Admin;
 class Student;
+class Course;
 
 class GlobalData {
 public:
     std::unordered_map<int, Admin> admin;
+    std::unordered_map<int, Course> courses;
     std::unordered_map<std::string, Student> students;
 };
