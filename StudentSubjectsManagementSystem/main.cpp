aa9597b559313890a34db03ffe359768798d76af
wasfyelbaz <wasfylbaz@gmail.com>
2022-05-13 17:37:46 +0200

    Updated main to include save gd method

--- StudentSubjectsManagementSystem/main.cpp
+++ StudentSubjectsManagementSystem/main.cpp
@@ -21,20 +21,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     dbh.load_admin_in_memory(gd.admin);
     dbh.load_students_in_memory(gd.students);
     dbh.load_courses_in_memory(gd.courses);
-
+    // print to console
     for (auto& admin : gd.admin) {
         std::cout << "Info: Loaded from in memory hashtables, admin: " << admin.second.get_name() << std::endl;
     }
-
     for (auto& x : gd.students) {
         std::cout << "Info: Loaded from in memory hashtables, student: " << x.second.get_name() << std::endl;
         std::cout << "Info: Loaded from in memory hashtables, number of finished courses: " << x.second.finished_courses.size() << std::endl;
     }
-
     for (auto& course : gd.courses) {
         std::cout << "Info: Loaded from in memory hashtables, course: " << course.second.get_name() << std::endl;
     }
-
+    // save changed data back to the database
+    dbh.save_data_to_db(gd);
+    // pause console
     std::string pause;
     std::cin >> pause;
     return 0;
